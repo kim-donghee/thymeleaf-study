35b1926f0161ce71cb6ec033c26f4a6c61c57c27
KIM <KIM@DESKTOP-OMKLPTR>
2019-08-01 15:00:00 +0900

    attr / checked

--- src/main/java/me/dong/thymeleafstudy/HomeController.java
+++ src/main/java/me/dong/thymeleafstudy/HomeController.java
@@ -13,6 +13,7 @@ public class HomeController {
 	public String index(Model model) {
 		model.addAttribute("message", "Hello Dong");
 		model.addAttribute("account", Account.builder().name("dong").tel("010-0000-0000").build());
+		model.addAttribute("checked", true);
 		return "index";
 	}
 	
