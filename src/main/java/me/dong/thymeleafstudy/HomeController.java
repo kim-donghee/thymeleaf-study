642bf992c2ded1a33bfe6ca89fb60eff6d6775d2
KIM <KIM@DESKTOP-OMKLPTR>
2019-08-01 16:00:43 +0900

    footer

--- src/main/java/me/dong/thymeleafstudy/HomeController.java
+++ src/main/java/me/dong/thymeleafstudy/HomeController.java
@@ -1,5 +1,9 @@
 package me.dong.thymeleafstudy;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +18,20 @@ public class HomeController {
 		model.addAttribute("message", "Hello Dong");
 		model.addAttribute("account", Account.builder().name("dong").tel("010-0000-0000").build());
 		model.addAttribute("checked", true);
+//		model.addAttribute("accounts", generateAccounts());
+		model.addAttribute("sw", 1);
 		return "index";
+	}
+	
+	private Set<Account> generateAccounts() {
+		
+		Set<Account> accounts = new HashSet<>();
+		
+		IntStream.range(0, 30).forEach(i -> {
+			accounts.add(Account.builder().id(i).name("dong" + i).tel("010-0000-0000").build());
+		});
+		
+		return accounts;
 	}
 	
 }
--- src/main/java/me/dong/thymeleafstudy/account/Account.java
+++ src/main/java/me/dong/thymeleafstudy/account/Account.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(of = "id")
 public class Account {
 	
-	private String id;
+	private int id;
 	
 	private String name;
 	
