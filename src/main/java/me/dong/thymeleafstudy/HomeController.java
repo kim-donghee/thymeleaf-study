4111613d1b75ddcbd8585cd52f5420d10c2120a5
KIM <KIM@DESKTOP-OMKLPTR>
2019-08-01 14:31:08 +0900

    selection variable expression

--- src/main/java/me/dong/thymeleafstudy/HomeController.java
+++ src/main/java/me/dong/thymeleafstudy/HomeController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import me.dong.thymeleafstudy.account.Account;
+
 @Controller
 public class HomeController {
 	
 	@GetMapping
 	public String index(Model model) {
 		model.addAttribute("message", "Hello Dong");
+		model.addAttribute("account", Account.builder().name("dong").tel("010-0000-0000").build());
 		return "index";
 	}
 	
