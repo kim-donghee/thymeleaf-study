e74b15477a9ee7b24504a76187a4616a8b7d878d
KIM <KIM@DESKTOP-OMKLPTR>
2019-08-01 14:28:27 +0900

    account domain

---
+++ src/main/java/me/dong/thymeleafstudy/account/Account.java
@@ -0,0 +1,21 @@
+package me.dong.thymeleafstudy.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(of = "id")
+public class Account {
+	
+	private String id;
+	
+	private String name;
+	
+	private String tel;
+	
+
+}
